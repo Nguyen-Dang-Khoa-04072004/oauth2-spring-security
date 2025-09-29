dc079215245017aad561ddb6f8e78a1476ea1939
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-09-29 10:54:25 +0700

    feat: customer userinfo reponse

--- authorization-server/src/main/java/com/fpt/authorization_server/SecurityConfig.java
+++ authorization-server/src/main/java/com/fpt/authorization_server/SecurityConfig.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,18 +17,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationContext;
+import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcUserInfoAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.token.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import java.util.function.Function;
 
-import java.util.UUID;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @RequiredArgsConstructor
@@ -43,6 +46,15 @@ public class SecurityConfig {
     @Order(1)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
         throws Exception {
+
+        Function<OidcUserInfoAuthenticationContext, OidcUserInfo> userInfoMapper = (context) -> {
+            OidcUserInfoAuthenticationToken authentication = context.getAuthentication();
+            JwtAuthenticationToken principal = (JwtAuthenticationToken) authentication.getPrincipal();
+
+            return new OidcUserInfo(principal.getToken().getClaims());
+        };
+
+
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
             OAuth2AuthorizationServerConfigurer.authorizationServer();
 
@@ -50,14 +62,14 @@ public class SecurityConfig {
             .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
             .with(authorizationServerConfigurer, (authorizationServer) ->
                 authorizationServer
-                    .oidc(Customizer.withDefaults())	// Enable OpenID Connect 1.0
+                    .oidc(oidc -> {
+                        oidc.userInfoEndpoint(userInfo -> userInfo.userInfoMapper(userInfoMapper));
+                    })
             )
             .authorizeHttpRequests((authorize) ->
                 authorize
                     .anyRequest().authenticated()
             )
-            // Redirect to the login page when not authenticated from the
-            // authorization endpoint
             .exceptionHandling((exceptions) -> exceptions
                 .defaultAuthenticationEntryPointFor(
                     new LoginUrlAuthenticationEntryPoint("/login"),
@@ -76,41 +88,62 @@ public class SecurityConfig {
             .authorizeHttpRequests((authorize) -> authorize
                 .anyRequest().authenticated()
             )
-            // Form login handles the redirect to the login page from the
-            // authorization server filter chain
             .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Bean
-    UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService(){
         return username -> userRepository.findByUsername(username);
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    RegisteredClientRepository registeredClientRepository(){
-        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+    public RegisteredClientRepository registeredClientRepository(){
+        RegisteredClient registeredClient = RegisteredClient.withId("spring")
             .clientId("client")
             .clientSecret(passwordEncoder().encode("secrets"))
             .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
             .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
             .scope(OidcScopes.OPENID)
-            .redirectUri("https://spring.io/guides/gs/accessing-data-mysql")
+            .redirectUri("https://docs.spring.io/spring-security/reference/index.html")
             .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
             .build();
         return new InMemoryRegisteredClientRepository(registeredClient);
     }
 
     @Bean
-    JWKSource<SecurityContext> jwkSource(){
+    public JWKSource<SecurityContext> jwkSource(){
         JWKSet jwkSet = new JWKSet(keyManager.rsaKey());
         return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
     }
 
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    // Customize Jwt Token
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer(){
+        return context -> {
+            if(OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())){
+                context.getClaims().claims(claims -> {
+                    User user = (User) context.getPrincipal().getPrincipal();
+                    claims.put("sub",user.getId());
+                    claims.put("username",user.getUsername());
+                    claims.put("email",user.getEmail());
+                    claims.put("imageProfile",user.getProfileImageUrl());
+                    claims.remove("aud");
+                    claims.remove("nbf");
+                });
+            }
+        };
+    }
 }
