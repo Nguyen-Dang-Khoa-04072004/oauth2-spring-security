781b71d02c50d3573e333ce129766147487f23b0
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-10-01 14:32:09 +0700

    feat: update authorizationsever config

--- authorization-server/src/main/java/com/fpt/authorization_server/SecurityConfig.java
+++ authorization-server/src/main/java/com/fpt/authorization_server/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
                 authorize
                     .anyRequest().authenticated()
             )
+            .formLogin(Customizer.withDefaults())
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .exceptionHandling((exceptions) -> exceptions
                 .defaultAuthenticationEntryPointFor(
