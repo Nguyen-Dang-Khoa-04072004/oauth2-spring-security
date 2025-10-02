cf695f15b88e4842fdad55d3dd73f683172dabf0
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-10-02 11:55:06 +0700

    feat: update css style

--- client-app/src/component/header/Header.jsx
+++ client-app/src/component/header/Header.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import "./header.css";
-import logoImg from "../../../public/logo.png"
+import logoImg from "../../../public/logo.png";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import { BsSearch } from "react-icons/bs";
 import { LuShoppingCart } from "react-icons/lu";
@@ -9,20 +9,21 @@ function Header() {
   return (
     <div className="header">
       <div className="header__main">
-          <div className="header__logo">
-            <HiOutlineShoppingCart color="white" size={35}/>
-            <img
-              src={logoImg}
-              alt=""
+        <div className="header__logo">
+          <HiOutlineShoppingCart color="white" size={35} />
+          <img src={logoImg} alt="" />
+        </div>
+        <div className="header__search-bar">
+          <div className="search-bar__input-bar">
+            <BsSearch size={20} color="#F37021" style={{ cursor: "pointer" }} />
+            <input type="text" placeholder="Nhập tên sản phẩm cần tìm" />
+            <LuShoppingCart
+              size={25}
+              color="#F37021"
+              style={{ cursor: "pointer" }}
             />
           </div>
-          <div className="header__search-bar">
-            <div className="search-bar__input-bar">
-              <BsSearch size={20} color="#F37021" style={{cursor: 'pointer'}}/>
-              <input type="text" placeholder="Nhập tên sản phẩm cần tìm"/>
-              <LuShoppingCart size={25} color="#F37021" style={{cursor: 'pointer'}}/>
-            </div>
-          </div>
+        </div>
         <div className="header__userinfo">
           <div className="header__username">{user?.username}</div>
           <div className="header__image-profile">
