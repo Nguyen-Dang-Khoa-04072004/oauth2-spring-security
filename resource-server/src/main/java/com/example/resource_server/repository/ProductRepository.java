184183249d4d6d47e06adcba7630de2ef324089f
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-10-06 13:47:19 +0700

    chores: uncomment some methods

--- resource-server/src/main/java/com/example/resource_server/repository/ProductRepository.java
+++ resource-server/src/main/java/com/example/resource_server/repository/ProductRepository.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-//    @Query("SELECT p FROM Product p WHERE p.subCategory.id IN (SELECT sc.id FROM SubCategory sc WHERE sc.category = ?1)")
-//    List<Product> getAllProductByCategoryId(Long categoryId);
-//    @Query("SELECT p FROM Product p WHERE p.subCategory.id = ?1")
-//    List<Product> getAllProductBySubCategoryId(Long subCategoryId);
+    @Query("SELECT p FROM Product p WHERE p.subCategory.id IN (SELECT sc.id FROM SubCategory sc WHERE sc.category = ?1)")
+    List<Product> getAllProductByCategoryId(Long categoryId);
+    @Query("SELECT p FROM Product p WHERE p.subCategory.id = ?1")
+    List<Product> getAllProductBySubCategoryId(Long subCategoryId);
 }
