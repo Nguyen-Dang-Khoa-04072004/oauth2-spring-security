1008b2a305b1eeccd23f0006163c28880c0086d8
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-10-06 13:42:59 +0700

    feat: add find product by category and sub category method

--- resource-server/src/main/java/com/example/resource_server/repository/ProductRepository.java
+++ resource-server/src/main/java/com/example/resource_server/repository/ProductRepository.java
@@ -2,8 +2,14 @@ package com.example.resource_server.repository;
 
 import com.example.resource_server.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+//    @Query("SELECT p FROM Product p WHERE p.subCategory.id IN (SELECT sc.id FROM SubCategory sc WHERE sc.category = ?1)")
+//    List<Product> getAllProductByCategoryId(Long categoryId);
+//    @Query("SELECT p FROM Product p WHERE p.subCategory.id = ?1")
+//    List<Product> getAllProductBySubCategoryId(Long subCategoryId);
 }
